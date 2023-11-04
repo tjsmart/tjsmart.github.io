3f89454e3b8e19ec51bf6a66ff00d8dfa8340ba3
Tyler Smart <tjsmart@ucsc.edu>
2023-11-04 17:04:30 -0600

    Add favicon and update background colors to always be dark

--- app/components/prompt.tsx
+++ app/components/prompt.tsx
@@ -2,7 +2,7 @@ export function Prompt() {
     return (
         <span className="whitespace-pre">
             <span className="text-green-500">earthling@tjsmart.github.io</span>
-            {" > "}
+            {" ❯ "}
         </span>
     );
 }
