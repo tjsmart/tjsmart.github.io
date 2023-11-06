3a2d6a4ef58be6283c3951288f93c7b699bdf90d
Tyler Smart <tjsmart@ucsc.edu>
2023-11-05 19:56:01 -0700

    Can provide contact kind to contact command and it will open href

--- app/components/commands/contact.test.tsx
+++ app/components/commands/contact.test.tsx
@@ -2,8 +2,17 @@ import renderer from "react-test-renderer";
 import { Command } from ".";
 import * as mut from "./contact";
 
+type Contact = {
+    kind: string;
+    username: string;
+    href: string;
+    icon: string;
+};
+
 // @ts-ignore
 const contact: Command = mut.contact;
+// @ts-ignore
+const contacts: Contact[] = mut.contacts;
 
 describe("contact", () => {
     test("contact snapshot", () => {
@@ -15,5 +24,16 @@ describe("contact", () => {
         const component = renderer.create(result);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    test("specific contacts opens href", () => {
+        const mock_open = jest.fn();
+        window.open = mock_open;
+
+        contacts.forEach((c, index) => {
+            contact.callback([c.kind]);
+            expect(mock_open.mock.calls).toHaveLength(index + 1);
+            expect(mock_open.mock.calls[index][0]).toStrictEqual(c.href);
+        });
     });
 });
--- app/components/commands/contact.tsx
+++ app/components/commands/contact.tsx
@@ -29,31 +29,45 @@ const contacts: Contact[] = [
     {
         kind: "google scholar",
         username: "Tyler Smart",
-        href: "https://www.scholar.google.com/citations?hl=en&user=r9supmkAAAAJ",
+        href: "https://scholar.google.com/citations?user=r9supmkAAAAJ",
         icon: "graduation-cap",
     },
 ];
 
 const contact = new Command(
     "contact",
-    () => {
-        return (
-            <>
-                {contacts.map((contact, index) => (
-                    <div key={index}>
-                        <a
-                            href={contact.href}
-                            className="hover:text-red-500 underline"
-                        >
-                            <i className={`fa fa-${contact.icon}`}></i>
-                            {` ${contact.kind} - ${contact.username}`}
-                        </a>
+    (args) => {
+        if (args.length == 0) {
+            return (
+                <>
+                    {contacts.map((contact, index) => (
+                        <div key={index}>
+                            <a
+                                href={contact.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="hover:text-red-500 underline"
+                            >
+                                <i className={`fa fa-${contact.icon}`}></i>
+                                {` ${contact.kind} - ${contact.username}`}
+                            </a>
 
-                        <br />
-                    </div>
-                ))}
-            </>
-        );
+                            <br />
+                        </div>
+                    ))}
+                </>
+            );
+        } else if (args.length == 1) {
+            const [arg] = args;
+            let selectedContact = contacts.find(({ kind }) => kind == arg);
+            if (selectedContact) {
+                window.open(selectedContact.href, "_blank");
+                return "";
+            }
+            return `error: unknown contact '${arg}'`;
+        } else {
+            return "error: contact takes at most a single argument";
+        }
     },
     `How to contact/follow me, e.g., ${contacts
         .slice(0, 2)
@@ -65,4 +79,5 @@ registerCommand(contact);
 
 if (process.env["NODE_DEV"] == "TEST") {
     module.exports.contact = contact;
+    module.exports.contacts = contacts;
 }
